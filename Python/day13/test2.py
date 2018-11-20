0654ab67a4b7cca1582ec3a1df5c3b8ee0bd1b63
tauovir.khan <tauovir.khan@appinventiv.com>
2018-11-20 22:44:38 +0530

    update

--- Python/day13/test2.py
+++ Python/day13/test2.py
@@ -5,37 +5,36 @@ data = pd.read_csv("emp.csv")
 #print data
 doc = dm.Document()
 rootElement = doc.createElement('employees')
-
-for df in data:
-    print data[df][0]
-
-""""
-for roll,name in data['eno']:
+#print((data['ename']))
+data = data.as_matrix()    # Make as two diamention array
+for item in data:
     empElement = doc.createElement('employee')
-    rollEle = doc.createElement('roll')
-    rollText = doc.createTextNode(str(roll))
-    rollEle.appendChild(rollText)
-    empElement.appendChild(rollEle)
+    #Make <id>101</id>
+    idElement = doc.createElement('id')
+    idText = doc.createTextNode(str(item[0]))
+    idElement.appendChild(idText)
+    #Make <name>Anil</name>
+    nameElement = doc.createElement('name')
+    nameText = doc.createTextNode(item[1])
+    nameElement.appendChild(nameText)
+    #Make <designation>scientist</designation>
+    designationElement = doc.createElement('designation')
+    designationText = doc.createTextNode(item[2])
+    designationElement.appendChild(designationText)
+    #<salary>50000</salary>
+    salaryElement = doc.createElement('salary')
+    salaryText = doc.createTextNode(str(item[3]))
+    salaryElement.appendChild(salaryText)
+    #Append All Element
+    empElement.appendChild(idElement)
+    empElement.appendChild(nameElement)
+    empElement.appendChild(designationElement)
+    empElement.appendChild(salaryElement)
     rootElement.appendChild(empElement)
-    """
-    
-name = doc.createElement('name')
-"""
-rollText = doc.createTextNode(name)
-roll.appendChild(rollText)
-    
-nameText = doc.createTextNode()
-
-designation = doc.createElement('designation')
-designationText = doc.createTextNode()
-
-salary = doc.createElement('salary')
-salaryText = doc.createTextNode()
-"""
-#rootElement.appendChild(rollEle)
+#Append all element to doc element
 doc.appendChild(rootElement)
 fd = open('f2.xml','w')
-doc.writexml(fd)
+doc.writexml(fd, newl='\n')
 fd.close()
 
 
