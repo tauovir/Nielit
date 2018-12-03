bbe375e40feef67e4d4c155544af92e579dc0a35
tauovir.khan <tauovir.khan@appinventiv.com>
2018-12-03 22:33:31 +0530

    update

--- Python/day24/day24.py
+++ Python/day24/day24.py
@@ -1,7 +1,6 @@
 import  pandas as pd
 import numpy as np
 from pandas import DataFrame as frm
-print "khan"
 
 arr = np.random.randint(10,20,40)
 arr = arr.reshape(10,4)
@@ -14,24 +13,73 @@ dataFrame = dataFrame.sort_values('c1')
 dataFrame = dataFrame.sort_values('c3',ascending = True)
 #print "=========Descending Order=========="
 #print dataFrame
-#========================Data Frame choise=====================
+#========================Data Frame choise Question=====================
 
 class Demo:
     def __init__(self):
-        dataset = [['BMW','INDIA',5000],['AUDI','CANADA',7000],['MARUTI','U.S',80000],['SKODA','NEWZEALAND',9000]]
+        dataset = [['BMW','INDIA',5000],
+        ['AUDI','CANADA',7000],
+        ['MARUTI','U.S',80000],
+        ['SKODA','NEWZEALAND',9000]
+        ]
         a1 = np.array(dataset).reshape(4,3)
         self.dataFrame = frm(a1,columns = ['Item','Place','totalSale'])
-        print self.dataFrame
+        print (len(self.dataFrame))
+    # Description: Add New Data to Dataframe
     def addData(self):
         item  = input("Enter Item Name:")
         place = input("enter Country name:")
         sale = input('enter sale amount:')
         list1 = [item,place,sale]
-        
-        self.dataFrame.iloc[-1] = list1
-        self.dataFrame.index = self.dataFrame.index + 1
-        print self.dataFrame
+        arr1 = np.array([list1])
+        newDataFrame = frm(arr1,columns = ['Item','Place','totalSale'],index = [len(self.dataFrame)])
+        self.dataFrame = self.dataFrame.append(newDataFrame)
+        print (self.dataFrame)
+    # Description: Get rank of given Item by place wise
+    def placeWiseRank(self):
+        ItemName  = input("enter Item name:")
+        print(self.dataFrame.rank)
+        self.dataFrame['pRank'] = self.dataFrame['Place'].rank()
+        print("========Placewise rank=========")
+        print(self.dataFrame)
+        rank = 0
+        for i in range(len(self.dataFrame)):
+            if self.dataFrame['Item'][i] == ItemName:
+                rank = self.dataFrame['pRank'][i]
+                break
+        print("Rank of Item:" + ItemName + ":"+str(rank))
 
+   # Description: Get rank of given Item by Item wise
+    def itemWiseRank(self):
+        ItemName  = input("enter Item name:")
+        print(self.dataFrame.rank)
+        self.dataFrame['iRank'] = self.dataFrame['Item'].rank()
+        print("========Item wise rank=========")
+        print(self.dataFrame)
+        for i in range(len(self.dataFrame)):
+            if self.dataFrame['Item'][i] == ItemName:
+                rank = self.dataFrame['iRank'][i]
+                break
+        print("Rank of Item:" + ItemName + ":"+str(rank))
 
 s1 = Demo()
-s1.addData()
+while(True):
+    print ("==================================")
+    print ("1 : Add Data")
+    print ("2 : Place wise rank")
+    print ("3 : Item wise rank")
+    print ("7  Exit")
+    key1 = input("Enter Your Choise:")
+    key1 = int(key1)
+    if key1 == 1:
+        s1.addData()
+    elif key1 == 2:
+        s1.placeWiseRank()
+    elif key1 == 3:
+        s1.itemWiseRank()
+    elif key1 == 7:
+        print ("Bye Bye")
+        break;
+    else:
+        print ("enter above charecter")
+    
