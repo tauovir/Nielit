481306b89afbeecc0e8fd861256d3bc93a10c9d3
AI20 <taukir707@gmail.com>
2019-01-03 09:50:11 +0530

    update

---
+++ TensorFlow/test1.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from tensorflow import keras
+
+#Helper library
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+#import mnst datasets of fations
+#fashion_mnist = keras.datasets.fashion_mnist
+
+#(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+
+
